895f0555a6a90074a0b8e414b8ec1cccae81a0a4
erbester51 <55757768+erbester51@users.noreply.github.com>
2024-10-27 19:25:46 -0500

    Added  class Calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,19 @@
 import tkinter as tk
 
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
+SMALL_FONT_STYLE = ("Arial", 16)
+DIGITS_FONT_STYLE = ("Arial", 24, "bold")
+DEFAULT_FONT_STYLE = ("Arial", 20)
+
+OFF_WHITE = "#F8FAFF"
+WHITE = "#FFFFFF"
+LIGHT_BLUE = "#CCEDFF"
+LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
+
+class Calculator:
+    def _init_(self):
+        self.window = tk.tk()
+        self.window.geometry("375X667")
+        self.window.resizable(0, 0)
+        self.window.title("Calculator")
